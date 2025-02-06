d152eaa5f844815267754682c44c0889a70eac4e
henriqueCostaSI <henrique.mcosta@hotmail.com>
2025-02-06 19:39:19 -0300

    Atualizando o Css

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Main from '../template/Main'
 
 const Home = props => (
-    <Main icon="home" title="Início" subtitle="Segundo Projeto do capítulo de React.">
+    <Main icon="home" title="Início">
         <div className='display-4'>Bem Vindo!</div>
         <hr />
         <p className="mb-0">Sistema para exemplificar a construção
--- frontend/src/components/template/Header.jsx
+++ frontend/src/components/template/Header.jsx
@@ -6,7 +6,7 @@ const Header = props => (
         <h1 className="mt-3">
             <i className={`fa fa-${props.icon}`}></i> {props.title}
         </h1>
-        <p className="lead text-muted">{props.subtitle}</p>
+        
     </header>
 )
 
