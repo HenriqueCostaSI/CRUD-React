f0711427c4b4a5ca159e82f45a54517aaca01556
henriqueCostaSI <henrique.mcosta@hotmail.com>
2025-02-06 20:14:17 -0300

    atualiza as dependências do react-router-dom para a versão 6.29.0 e ajusta as versões do react-router

--- frontend/src/components/template/Main.jsx
+++ frontend/src/components/template/Main.jsx
@@ -6,7 +6,9 @@ const Main = (props) => (
   <React.Fragment>
     <Header {...props} />
     <main className="content container-fluid">
-      <div className="p-3 mt-3">{props.children}</div>
+      <div className="p-3 mt-3">
+        {props.children}
+      </div>
     </main>
   </React.Fragment>
 );
--- frontend/src/main/Routes.jsx
+++ frontend/src/main/Routes.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import { Routes, Route, Navigate } from 'react-router'
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import Home from '../components/home/Home'
-import UserCrud from '../components/user/UserCrud'
+import Home from "../components/home/Home";
+import UserCrud from "../components/user/UserCrud";
 
-export default props => 
-    <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route path='/users' element={<UserCrud />} />
-        <Route path='*' element={<Navigate to='/' />} />
-    </Routes>
+export default (props) => (
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/users" element={<UserCrud />} />
+    <Route path="*" element={<Navigate to="/" />} />
+  </Routes>
+);
